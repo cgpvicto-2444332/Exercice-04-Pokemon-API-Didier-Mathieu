15eed35142e5ebf551f65151726cd3a15fe8c2de
Didier Mathieu <dmathieu2007@gmail.com>
2026-02-11 13:46:23 -0500

    ajout des fichiers de structure de base

---
+++ server.js
@@ -0,0 +1,17 @@
+import express from 'express';
+import dotenv from 'dotenv';
+
+dotenv.config();
+
+const app = express();
+const port = process.env.PORT;
+
+app.use(express.json());
+
+app.get('/', (req, res) => {
+    res.json({ message: "Bienvenue à l'API de pokémon!" });
+});
+
+app.listen(port, () => {
+    console.log(`Serveur démarré sur le port ${port}`);
+});
