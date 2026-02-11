c54ed807a6dc2b66f3e3f2276925952116bf99ca
Didier Mathieu <dmathieu2007@gmail.com>
2026-02-11 14:30:52 -0500

    implémentation de la doc non complétée

--- server.js
+++ server.js
@@ -1,16 +1,25 @@
 import express from 'express';
 import dotenv from 'dotenv';
+import swaggerUi from 'swagger-ui-express';
+import fs from 'fs';
+
+const swaggerDocument = JSON.parse(fs.readFileSync('./src/config/documentation.json', 'utf8'));
 
 dotenv.config();
 
+const swaggerOptions = {
+    customCss: '.swagger-ui .topbar { display: none }',
+    customSiteTitle: "Pokemon API"
+};
+
 const app = express();
-const port = process.env.PORT;
+const port = process.env.PORT || 3030;
 
 app.use(express.json());
 
-app.get('/', (req, res) => {
-    res.json({ message: "Bienvenue à l'API de pokémon!" });
-});
+app.use('/api/docs',
+        swaggerUi.serve,
+        swaggerUi.setup(swaggerDocument, swaggerOptions));
 
 app.listen(port, () => {
     console.log(`Serveur démarré sur le port ${port}`);
